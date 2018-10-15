d555817d27ee47e184e8967f032dece2d02ff9f1
m5r <me@mokhtarmial.com>
2018-10-15 17:26:52 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 
 class App extends Component {
+	state = {
+		data: [
+			{ name: 'One', one: 0 },
+			{ name: 'Five', five: 0 },
+		],
+	};
+
+	componentWillMount() {
+		setInterval(
+			() => this.fetchData(),
+			5000,
+		);
+	}
+
+	async fetchData() {
+		const results = await (await fetch('https://analytics.mokhtarmial.com/results')).json();
+		const data = [
+			{ name: 'One', one: results.one },
+			{ name: 'Five', five: results.five },
+		];
+		this.setState({ data });
+	}
+
   render() {
+		const { data } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+	        <p>Results</p>
+	        <BarChart width={400} height={300} data={data}>
+		        <CartesianGrid strokeDasharray="3 3"/>
+		        <XAxis dataKey="name"/>
+		        <YAxis/>
+		        <Tooltip/>
+		        <Bar dataKey="one" fill="#8884d8" />
+		        <Bar dataKey="five" fill="#82ca9d" />
+	        </BarChart>
         </header>
       </div>
     );
